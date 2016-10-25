fcd7276f6cd512dec06aca744944caadc9fb5209
limailmirino <telefonosuci@gmail.com>
2016-10-25 11:48:45 +0200

    Removed @override from uservalidator method

--- src/main/java/it/bamboolab/validator/UserValidator.java
+++ src/main/java/it/bamboolab/validator/UserValidator.java
@@ -7,12 +7,12 @@ import org.springframework.validation.Validator;
  * Created by Enrico on 11/11/2015.
  */
 public class UserValidator implements Validator {
-    @Override
-    public boolean supports(Class<?> aClass) {
+    
+	public boolean supports(Class<?> aClass) {
         return false;
     }
 
-    @Override
+    
     public void validate(Object o, Errors errors) {
 
     }
